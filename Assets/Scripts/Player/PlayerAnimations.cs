0ebc25a6b2007e1009558a7efd8f4c35e29b0186
Juanda999 <juanda999games@gmail.com>
2025-10-28 19:25:50 -0500

    Add jumping logic

--- Assets/Scripts/Player/PlayerAnimations.cs
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -8,13 +8,20 @@ public class PlayerAnimations : MonoBehaviour
     void Start()
     {
         InputActionManager.instance.GetHorizontalAxisAction().started += x => SetMovingAnimation(x,true);
-        InputActionManager.instance.GetHorizontalAxisAction().canceled+= x => SetMovingAnimation(x,false);
+        InputActionManager.instance.GetHorizontalAxisAction().canceled += x => SetMovingAnimation(x, false);
+
+        PlayerMovement.OnPlayerGroundStatus += SetJumpAnimation;
     }
 
-    private void SetMovingAnimation(InputAction.CallbackContext callbackContext,bool isMoving)
+    private void SetMovingAnimation(InputAction.CallbackContext callbackContext, bool isMoving)
     {
         playerAnimator.SetBool("Moving", isMoving);
 
-        transform.localScale = new Vector2(callbackContext.ReadValue<float>(), transform.localScale.y);
+        transform.localScale = new Vector2(isMoving ? callbackContext.ReadValue<float>() : transform.localScale.x, transform.localScale.y);
+    }
+
+    public void SetJumpAnimation(bool value)
+    {
+        playerAnimator.SetBool("Air", value);
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,45 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D playerRb;
     [SerializeField] private float movementSpeed;
 
+    [Header("Jump Settings")]
+    [SerializeField] private Transform jumpDetectionPos;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float jumpDetectionRadius;
+    [SerializeField] private float jumpHeight;
+    private bool canJump;
+    public static event Action<bool> OnPlayerGroundStatus;
+
     private float playerAxis;
+
+    void Start()
+    {
+        InputActionManager.instance.GetJumpAction().started += _=>Jump();
+    }
+
     void Update()
     {
         playerAxis = InputActionManager.instance.GetHorizontalAxisAction().ReadValue<float>();
+
+        OnPlayerGroundStatus?.Invoke(!canJump);
     }
 
     void FixedUpdate()
     {
         playerRb.velocity = new Vector2(playerAxis * movementSpeed, playerRb.velocity.y);
+
+        canJump = Physics2D.OverlapCircle(jumpDetectionPos.position, jumpDetectionRadius, groundLayer);
+    }
+    private void Jump()
+    {
+        if (canJump)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpHeight);
+        }
     }
 }
