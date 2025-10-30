920e54aa937d8caaa81e85e17a8a961e8741fe7b
Juanda999 <juanda999games@gmail.com>
2025-10-29 22:36:24 -0500

    Add logic for instantiating bullets

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -1,14 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
     public static bool isShooting;
-
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float timeBetweenBullets;
+    [SerializeField] private Transform generateBulletPos;
+    private float currentTimeBetweenBullets = 0;
     void Update()
     {
         isShooting = InputActionManager.instance.GetShootAction().ReadValue<float>() > 0;
+
+        if(!isShooting)
+        {
+            return;
+        }
+        if (currentTimeBetweenBullets > 0)
+        {
+            currentTimeBetweenBullets -= Time.deltaTime;
+        }
+        else
+        {
+            currentTimeBetweenBullets = timeBetweenBullets;
+            GenerateBullet();
+        }
+    }
+
+    private void GenerateBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, generateBulletPos.transform.position,Quaternion.identity);
     }
 
 }
