ce9abc8420f48eade34d1945b3201015995eb58f
Juanda999 <juanda999games@gmail.com>
2025-10-30 22:53:00 -0500

    Add script to control bullet rotation based on it´s initial speed

--- Assets/Scripts/Combat/BulletBehavior.cs
+++ Assets/Scripts/Combat/BulletBehavior.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,8 +10,39 @@ public class BulletBehavior : MonoBehaviour
     void Start()
     {
         transform.localScale = new Vector2(PlayerMovement.lastAxis, 1);
-        bulletRb.velocity = new Vector2(PlayerMovement.playerHorizontalAxis, PlayerMovement.playerVerticalAxis) * bulletSpeed;
-        Debug.Log(new Vector2(PlayerMovement.playerHorizontalAxis, PlayerMovement.playerVerticalAxis));
+        bulletRb.velocity = new Vector2(GetBulletSpeedDirection(), PlayerMovement.playerVerticalAxis) * bulletSpeed;
+
+
+        transform.localRotation = Quaternion.Euler(0, 0, GetZRotation());
+    }
+
+    private float GetBulletSpeedDirection()
+    {
+        if (PlayerMovement.playerHorizontalAxis == 0 && PlayerMovement.playerVerticalAxis == 1)
+        {
+            return 0;
+        }
+        else if (PlayerMovement.playerVerticalAxis == 0 && PlayerMovement.playerHorizontalAxis == 0)
+        {
+            return PlayerMovement.lastAxis;
+        }
+
+        return PlayerMovement.playerHorizontalAxis;
+    }
+
+    private float GetZRotation()
+    {
+        if (Mathf.Abs(PlayerMovement.playerHorizontalAxis) == 1 && Math.Abs(PlayerMovement.playerVerticalAxis) == 1)
+        {
+            return 45 * PlayerMovement.lastAxis;
+        }
+
+        if (PlayerMovement.playerVerticalAxis == 1 && Mathf.Abs(PlayerMovement.playerHorizontalAxis) == 0)
+        {
+            return 90;
+        }
+
+        return 0;
     }
 
 }
