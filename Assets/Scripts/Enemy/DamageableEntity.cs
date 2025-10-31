c0fe6d07cd2203e17fb32c030a05235f475307e4
Juanda999 <juanda999games@gmail.com>
2025-10-31 00:32:32 -0500

    Add method for checking if the target is death

--- Assets/Scripts/Enemy/DamageableEntity.cs
+++ Assets/Scripts/Enemy/DamageableEntity.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 public class DamageableEntity : MonoBehaviour
 {
     [SerializeField] private int entityHealth;
-
+    [SerializeField] private SpriteRenderer _renderer;
     public void TakeDamage(int damage)
     {
         entityHealth -= damage;
+
+        if(entityHealth<=0)
+        {
+            OnDeath();
+        }
+    }
+
+    private void OnDeath()
+    {
+        _renderer.color = Color.red;
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
