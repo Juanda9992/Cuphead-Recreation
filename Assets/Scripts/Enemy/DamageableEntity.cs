1e6110d2af35672461c539db0f3f3a42fe1e7e4e
Juanda999 <juanda999games@gmail.com>
2025-10-31 00:49:27 -0500

    Add tint on objects when taking damage

--- Assets/Scripts/Enemy/DamageableEntity.cs
+++ Assets/Scripts/Enemy/DamageableEntity.cs
@@ -1,17 +1,30 @@
 using UnityEngine;
-
+using DG.Tweening;
+using System.Collections;
 public class DamageableEntity : MonoBehaviour
 {
     [SerializeField] private int entityHealth;
     [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private GameObject animationMask;
+    [SerializeField] private SpriteRenderer damageTint;
     public void TakeDamage(int damage)
     {
         entityHealth -= damage;
 
-        if(entityHealth<=0)
+        StartCoroutine("DamageAnimation");
+
+        if (entityHealth <= 0)
         {
             OnDeath();
         }
+    }
+    
+    private IEnumerator DamageAnimation()
+    {
+        damageTint.DOFade(1, 0);
+        animationMask.SetActive(true);
+        yield return damageTint.DOFade(0, 0.1f).WaitForCompletion();
+        animationMask.SetActive(false);
     }
 
     private void OnDeath()
